55fd3c5db2b8e190e57b5a24e142597dd5305f80
T.J. Eckman <jeek@jeek.net>
2010-10-01 04:12:46 -0400

    YAML Conversion of Base Classes, Finished Spell Table Generator

--- Makefile
+++ Makefile
@@ -17,48 +17,61 @@ srd-html: out/srd/index.html
 srd-html-nochunks: out/srd.html
 
 out/srd.html: out/srd.xml
+	mkdir -p out
 	xmlto -o out html-nochunks out/srd.xml
 
 srd-ua-html-nochunks: out/srd-ua.html
 
 out/srd-ua.html: out/srd-ua.xml
+	mkdir -p out
 	xmlto -o out html-nochunks out/srd-ua.xml
 
 out/srd/index.html: out/srd.xml src/mychunk.xsl
+	mkdir -p out
 	xmlto -o out/srd xhtml out/srd.xml -m src/mychunk.xsl
 
 srd-pdf: out/srd.pdf
 
 out/srd.pdf: out/srd.xml
+	mkdir -p out
 	dblatex -o out/srd.pdf -t pdf out/srd.xml
 
 srd-epub: out/srd.epub
 
 out/srd.epub: out/srd.xml
+	mkdir -p out
 	dbtoepub -o out/srd.epub out/srd.xml
 
-out/srd.xml: src/*.asciidoc src/classes/*.asciidoc Makefile
-	asciidoc -a idprefix= -d book -b docbook -o out/srd.xml src/srd.asciidoc
+out/srd.xml: src/srd/*.asciidoc gen/srd/base-classes.asciidoc Makefile
+	mkdir -p out
+	asciidoc -a idprefix= -d book -b docbook -o out/srd.xml src/srd/srd.asciidoc
 
 srd-ua-html: out/srd-ua/index.html
 
 out/srd-ua/index.html: out/srd-ua.xml src/mychunk.xsl
+	mkdir -p out
 	xmlto -o out/srd-ua xhtml out/srd-ua.xml -m src/mychunk.xsl
 
 srd-ua-pdf: out/srd-ua.pdf
 
 out/srd-ua.pdf: out/srd-ua.xml
+	mkdir -p out
 	dblatex -o out/srd-ua.pdf -t pdf out/srd-ua.xml
 
 srd-ua-epub: out/srd-ua.epub
 
 out/srd-ua.epub: out/srd-ua.xml
+	mkdir -p out
 	dbtoepub -o out/srd-ua.epub out/srd-ua.xml
 
-out/srd-ua.xml: src/*.asciidoc src/classes/*.asciidoc Makefile
-	asciidoc -d book -a idprefix= -a unearthed-arcana -b docbook -o out/srd-ua.xml src/srd.asciidoc
+out/srd-ua.xml: src/srd/*.asciidoc gen/srd/base-classes.asciidoc Makefile
+	mkdir -p out
+	asciidoc -d book -a idprefix= -a unearthed-arcana -b docbook -o out/srd-ua.xml src/srd/srd.asciidoc
 
 clean:
-	rm -rf out/*
+	rm -rf out
+	rm -rf gen
 
-
+gen/srd/base-classes.asciidoc: src/srd/base-classes/*.yaml
+	mkdir -p gen/srd
+	python src/classes.py>gen/srd/base-classes.asciidoc
---
+++ src/classes.py
@@ -0,0 +1,135 @@
+import string
+import yaml
+import subprocess
+import os
+
+#classes=yaml.load(file('classes.yaml', 'r'))
+#classes=yaml.load(os.popen('cat srd/base-classes/*.yaml', 'r'))
+#(classfilesout,classfilesin)=subprocess.Popen3('cat srd/base-classes/*.yaml')
+
+#print '\nread:'
+proc = subprocess.Popen('cat src/*/base-classes/*.yaml',
+                       shell=True,
+                       stdout=subprocess.PIPE,
+                       )
+classfilesout = proc.communicate()[0]
+#print '\tstdout:', repr(stdout_value)
+
+
+classes=yaml.load(classfilesout)
+# print yaml.dump(classes)
+
+def saves(level, quality):
+  if (quality == "Poor"):
+    return int(level*1/3)
+  if (quality == "Good"):
+    return int(2+level/2)
+
+def bab(level, quality):
+  if (quality == "Poor"):
+    return int(level*.5)
+  if (quality == "Good"):
+    return level
+  if (quality == "Medium"):
+    return int(level*.75)
+
+def underline(symbol, text):
+  print text
+  line=""
+  for i in text:
+    line=line+symbol
+  print line
+  print
+
+underline("=","Base Classes")
+
+for i in classes:
+#for i in classes["Base Classes"]:
+#  for k in i:
+#    print k
+  underline("-",i["Name"])
+  underline("~","The " + i["Name"] + " Base Class")
+  if (i.has_key("Alignment")):
+    print "*Alignment:* "+i["Alignment"]
+    print  
+  print "*Hit Die:* "+i["Hit Die"]
+  print
+  underline("^","Class Skills")
+  print "*The "+i["Name"]+"'s class skills (and the key ability for each skill) are:* "
+  skills = i["Class Skills"].keys()
+  skills.sort()
+  skillsout=""
+#  print str.join(", ",skills)
+  for j in skills:
+    if (i["Class Skills"][j]):
+      skillsout=skillsout + j + " ("+i["Class Skills"][j]+"), "
+    else:
+      skillsout=skillsout + j + ", "
+  skillsout=skillsout.rstrip(", ")
+  print skillsout
+  print
+  print "*Skill Points at 1st Level:* ("+str(i["Skill Points Per Level"])+" + Int Modifier) x 4."
+  print
+  print "*Skill Points at Each Additional Level:* "+str(i["Skill Points Per Level"])+" + Int Modifier."
+  print
+  print ".The " + i["Name"]
+  if (i.has_key("Table Format")):
+    print "[options=\"header\","+i["Table Format"]+"]"
+  else:
+    print "[options=\"header\"]"
+  print "|====="
+
+#  for k in range(1,21):
+#    for j in i["Class Features"]:
+#      if (i["Class Features"][j]["Level"]==k):
+#        print k,j
+
+# Determine Max Spell Level
+  maxspelllevel=-1
+  if (i.has_key('Spells Per Day')):
+    for j in i["Spells Per Day"]:
+      for k in i["Spells Per Day"][j]:
+        if (k>maxspelllevel):
+          maxspelllevel=k
+  print "| Level | Base Attack Bonus | Fort Save | Ref Save | Will Save | Special ",
+  if (maxspelllevel>-1):
+    for j in range(0,maxspelllevel+1):
+      print " | "+str(j),
+
+  print
+  
+  for j in range(1,21):
+    features=""
+    for k in i["Class Features"]:
+      if (i["Class Features"][k]["Level"]==j):
+        if (i["Class Features"][k].has_key("Short")):
+          features=features+i["Class Features"][k]["Short"]+", "
+
+    print "| " + str(j) + " | +" + str(bab(j,i["Base Attack Bonus"])) + " | +" + str(saves(j,i["Saves"]["Fortitude"])) + " | +" + str(saves(j,i["Saves"]["Reflex"])) + " | +" + str(saves(j,i["Saves"]["Will"])) + " | " + features.rstrip(", "),
+    if (i.has_key('Spells Per Day')):
+      for k in range(0,maxspelllevel+1):
+        if (i["Spells Per Day"][j].has_key(k)):
+          print " | ",
+          print i["Spells Per Day"][j][k],
+        else:
+          print " | - ",
+    print  
+    
+  print "|====="
+  print  
+
+  underline('^',"Class Features")
+
+  for k in range(0,21):
+    for j in i["Class Features"]:
+      if (i["Class Features"][j]["Level"]==k):
+        if (i["Class Features"][j].has_key("Description")):
+          print "*" + j + ":* " + i["Class Features"][j]["Description"].replace("\n","\n\n")
+          print
+        #["Description"]
+  print
+
+  if (i.has_key("Ex")):
+    underline("^","Ex-"+i["Name"]+"s")
+    print i["Ex"]["Description"]
+    print
